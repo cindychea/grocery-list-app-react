8dce1f667fb2f7334cc1138224f53ee816159b3c
cindychea <cindy_chea@hotmail.com>
2019-06-26 15:55:14 -0400

    naive way of updating value

--- grocery-list-react/src/Group.js
+++ grocery-list-react/src/Group.js
@@ -4,10 +4,10 @@ import Item from './Item';
 const Group = () => {
   return (
     <React.Fragment>
-      <Item group="meat" value="3" content="Steaks"/>
-      <Item group="prod" value="4" content="Apples"/>
-      <Item group="dairy" value="1" content="Milk (1L, 2%)"/>
-      <Item group="bakery" value="2" content="Baguettes"/>
+      <Item group="meat" value={3} content="Steaks"/>
+      <Item group="prod" value={4} content="Apples"/>
+      <Item group="dairy" value={1} content="Milk (1L, 2%)"/>
+      <Item group="bakery" value={2} content="Baguettes"/>
     </React.Fragment>
   )
 };
--- grocery-list-react/src/Item.js
+++ grocery-list-react/src/Item.js
@@ -4,15 +4,17 @@ const Item = ({group, value, content}) => {
   console.log(`Channel: value: ${value}`);
 
   const updateValue = (newValue) => {
-    console.log(`updating with ${newValue}`)
+    console.log(`old value ${value}`)
+    value = newValue;
+    console.log(`value after ${value}`)
   }
 
   return (
     <React.Fragment>
       <li className={ group }>
-        <button onClick={ () => updateValue(-1)  }>-</button>
+        <button onClick={ () => updateValue(value-1)  }>-</button>
           <span onChange={ ({target}) => updateValue(target.value) }>{ value } { content }</span>
-        <button onClick={ () => updateValue(1) }>+</button>
+        <button onClick={ () => updateValue(value+1) }>+</button>
       </li>
     </React.Fragment>
   )
